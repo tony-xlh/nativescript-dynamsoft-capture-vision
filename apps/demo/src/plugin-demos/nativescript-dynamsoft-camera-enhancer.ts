e96f7ae5d04a58c95ef65c0feed8cc9458a94e0b
xulihang <xulihanghai@163.com>
2022-12-30 15:57:52 +0800

    update layout

--- apps/demo/src/plugin-demos/nativescript-dynamsoft-camera-enhancer.ts
+++ apps/demo/src/plugin-demos/nativescript-dynamsoft-camera-enhancer.ts
@@ -7,6 +7,18 @@ export function navigatingTo(args: EventData) {
 	page.bindingContext = new DemoModel();
 }
 
+export function onSwitchTorch(args: EventData) {
+    alert("switch torch");
+}
+
+export function onSwitchCamera(args: EventData) {
+    alert("switch camera");
+}
+
+export function onCaptureFrame(args: EventData) {
+    alert("capture frame");
+}
+
 export class DemoModel extends DemoSharedNativescriptDynamsoftCameraEnhancer {
 	get isActive(): boolean {
 		return true;
--- packages/nativescript-dynamsoft-camera-enhancer/common.ts
+++ packages/nativescript-dynamsoft-camera-enhancer/common.ts
@@ -1,8 +1,17 @@
 import { Observable, Property, View } from '@nativescript/core';
 
+export declare class CameraEnhancer {
+    constructor();
+    switchTorch(on:boolean): Promise<boolean>;
+    selectCamera(id:string): Promise<boolean>;
+    opened(): Promise<void>;
+}
+
+
 export class DynamsoftCameraEnhancerCommon extends View {
 
 }
+
 export const activeProperty = new Property<DynamsoftCameraEnhancerCommon, boolean>({
   name: 'active',
 })
