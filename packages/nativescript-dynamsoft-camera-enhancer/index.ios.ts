168a6dccc7ce5a60605fd7f8e20e486a98160499
xulihang <xulihanghai@163.com>
2023-01-03 15:53:49 +0800

    ios implementation of Dynamsoft Camera Enhancer

--- packages/nativescript-dynamsoft-camera-enhancer/index.ios.ts
+++ packages/nativescript-dynamsoft-camera-enhancer/index.ios.ts
@@ -1,4 +1,4 @@
-import { CameraEnhancerCommon } from './common';
+import { activeProperty, CameraEnhancerCommon, cameraIDProperty, torchProperty } from './common';
 
 export class CameraEnhancer extends  CameraEnhancerCommon {
   dce:DynamsoftCameraEnhancer
@@ -17,5 +17,46 @@ export class CameraEnhancer extends  CameraEnhancerCommon {
 
   initNativeView() {
     
+  }
+
+  captureFrame(){
+    return this.dce.getFrameFromBuffer(true);
+  }
+
+  getAllCameras():string[]{
+    let array = [];
+    let cameras = this.dce.getAllCameras();
+    for (let index = 0; index < cameras.count; index++) {
+        const camera = cameras[index];
+        array.push(camera);
+    }
+    return array;
+  }
+
+  getSelectedCamera():string{
+    return this.dce.getSelectedCamera();
+  }
+
+  [activeProperty.setNative](value: boolean) {
+    if (value === true) {
+      this.dce.open();
+    }else{
+      this.dce.close();
+    }
+  }
+
+  [torchProperty.setNative](value: boolean) {
+    alert("torch property: "+value);
+    if (value === true) {
+      this.dce.turnOnTorch();
+    }else{
+      this.dce.turnOffTorch();
+    }
+  }
+
+  [cameraIDProperty.setNative](value: string) {
+    if (value) {
+      this.dce.selectCameraError(value);
+    }
   }
 }
