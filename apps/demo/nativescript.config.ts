fe31a03bfa3f07e8de4d0ec5deef7f5fe81de2b7
xulihang <xulihanghai@163.com>
2023-01-03 00:24:18 -0800

    update the app's bundle id

--- apps/demo/nativescript.config.ts
+++ apps/demo/nativescript.config.ts
@@ -1,7 +1,7 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-	id: 'org.nativescript.plugindemo',
+	id: 'com.xulihang.nativescript',
 	appResourcesPath: '../../tools/assets/App_Resources',
 	android: {
 		v8Flags: '--expose_gc',
@@ -9,6 +9,6 @@ export default {
 	},
 	appPath: 'src',
   cli: {
-    packageManager: 'npm' 
+    packageManager: 'npm'
   }
 } as NativeScriptConfig;
